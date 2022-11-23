986432b79afde429be295925e13f7f62349d4af0
Youssef Abdulaziz <azizy5203@gmail.com>
2022-11-23 06:45:17 +0200

    added /users /products routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const path = require("path");
 const express = require("express");
 const hbs = require('hbs')
+const { faker } = require('@faker-js/faker');
+const fs = require('fs')
+// import chalk from "chalk";
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000gi
 
 //Define paths for Express config
 const htmlPath = path.join(__dirname, "./public");
@@ -18,6 +21,41 @@ hbs.registerPartials(partialsPath)
 //serve static directory
 app.use(express.static(htmlPath));
 
+//generate random users
+const gnUsers = Array.from({ length: 10 }, (item, index) => {
+    return {
+        id: Math.floor(Math.random() * 31),
+        name: faker.name.fullName(),
+        email: faker.internet.email(),
+        token: faker.datatype.number({ min: 5000, max: 9000, precision: 1 }),
+        password:'123456'
+    }
+})
+
+//Overwrite db/users.json every time the app starts
+fs.writeFile(`../db/users.json`, JSON.stringify(gnUsers, null, 2), 'utf-8', (err) => {
+    if (err) console.log("[ERROR] Users not saved")
+    else console.log("[SUCCESS] Users saved successfully");
+})
+
+
+//generate products
+
+const gnProducts = Array.from({ length: 35 }, (item, index) => {
+    return {
+        id: Math.floor(Math.random() * 90),
+        name: faker.commerce.product(),
+        desc: faker.commerce.productDescription(),
+        company: faker.company.name(),
+    }
+})
+
+//Overwrite db/products.json every time the app starts
+fs.writeFile(`../db/products.json`, JSON.stringify(gnProducts, null, 2), 'utf-8', (err) => {
+    if (err) console.log("[ERROR] Products not saved")
+    else console.log("[SUCCESS] Products saved successfully");
+})
+
 //routes
 app.get("/", (req, res) => {
     res.render("index", {
@@ -31,10 +69,16 @@ app.get("/about", (req, res) => {
         email: "youssef@mail.com",
     });
 });
+app.get('/users',(req,res)=>{
+    res.send(gnUsers)
+})
+app.get('/products',(req,res)=>{
+    res.send(gnProducts)
+})
 app.get('*',(req,res)=>[
     res.render('404')
 ])
 app.listen(port, () => {
-    console.log("[Running] Server listening on port 3000");
+    console.log("[Running] Node server listening on port 3000");
     // console.log(htmlPath);
 });
